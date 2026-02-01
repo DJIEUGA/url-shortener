8a7194518abe9ed5c9189b655aae1c3ade33dbfa
Djieuga Djoubi <djieugadjoubirylenathan@gmail.com>
2026-02-01 21:01:48 +0100

    refactor: update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 
+import os
 from app import create_app, db
 from flask_migrate import Migrate
 
@@ -6,5 +7,8 @@ app = create_app()
 migrate = Migrate(app, db)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    if os.getenv('FLASK_ENV') != 'production':
+        app.run(debug=True)
+    else:
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
